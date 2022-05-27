4919d7b8ffd9377d51d20cfa859bacf9877bbc9b
saltydog1980 <mikeelambert99@gmail.com>
2022-05-26 21:07:00 -0400

    final code

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,5 +1,17 @@
 // How can you make this more scalable and reusable later?
 
-exports.findArmstrongNumbers = function() {
-
+exports.findArmstrongNumbers = function(arr) {
+    //empty array to push answer to
+    let ansArr = [];
+    /* creating an array taking original num to string then splitting them, then mapping and taking each num to pow of length and a final map to 
+    reduce the inner arrays taking whole thing back to one array filled with numbers same length as input array */
+    let testArr = arr.map(element => element.toString().split('').map((num, _, a) =>  num ** a.length)).map(elem => elem.reduce((a, b) => a + b) );
+    //looping through both arrays and pushing any matching cases to ansArray
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === testArr[i]) {
+            ansArr.push(arr[i])
+        }
+    }
+    return ansArr
 };
+
--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,23 @@
 # How can you make this more scalable and reusable later?
+import functools
 def find_armstrong_numbers(numbers):
+    base_list = []
+    ans_list = []
+    for x in numbers:
+        if len(str(x)) == 1:
+            base_list.append(x)
+        elif len(str(x)) == 2:
+            lis = list(map(int,str(x)))
+            lis2 = list(map(lambda item : item ** 2, lis))
+            lis3 = functools.reduce(lambda agg, item : agg + item, lis2)
+            base_list.append(lis3)
+        elif len(str(x)) == 3:
+            lis = list(map(int,str(x)))
+            lis2 = list(map(lambda item : item ** 3, lis))
+            lis3 = functools.reduce(lambda agg, item : agg + item, lis2)
+            base_list.append(lis3)
+    for x in range(0, len(numbers), 1):
+        if numbers[x] == base_list[x]:
+            ans_list.append(numbers[x])
+    return ans_list
+
